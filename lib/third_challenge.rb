238ede81a1899a0c673d3f71ef3c208dcbc0df12
Zoe Keller <keller.zoe98@gmail.com>
2020-06-09 19:57:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         },
       :matriarch => { name: "Lady Montague", age: "54"
         },
-      :hero => { name: "Romeo", age: "15"
+      :hero => { name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
@@ -14,7 +14,7 @@ def third_challenge
         },
       :matriarch => { name: "Lady Capulet", age: "50"
         },
-      :heroine => { name: "Juliet", age: 
+      :heroine => { name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
